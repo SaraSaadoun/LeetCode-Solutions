ece3bc9cc43025d32ee819c1660f823780247574
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-02 11:53:41 +0200

    Time: 9 ms (39.91%), Space: 10 MB (7.52%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        //maping their language to ours
+        unordered_map <char, char> mp;
+        
+        for(int i=0; i<26; ++i){
+            mp[order[i]] = i+'a';
+        }
+        
+        int n = (int)words.size();
+        
+        for(int i=0; i<n; ++i){
+            int m = words[i].size();
+            for(int j=0; j<m; ++j){
+                words[i][j] = mp[words[i][j]];
+            }
+        }
+
+        //get the sorted words
+        vector <string> sorted_words = words;
+        sort(sorted_words.begin(), sorted_words.end());
+
+        //compare
+        for(int i=0; i<n; ++i){
+            if(sorted_words[i] != words[i])
+                return false;
+        }
+
+        return true;
+        
+    }
+};
