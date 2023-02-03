0a820025594785b3e2d7aac35ac6679daf52af7e
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-03 04:37:41 +0200

    Time: 4 ms (95.37%), Space: 8.4 MB (62.80%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -2,20 +2,7 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        /*
-        c -> index of rows 
-        a pattern is deduced 
-            =>ex: s of i = 0, 1, 2, 3, 4, 5, 6, 7, .. and numRows = 4
-                       c = 0, 1, 2, 3, 2, 1, 0, 1, ..
-        
-            if c == 0         >> increment c till it reach numRows-1 (last row)
-            if c == numRows-1 >> decrement c till it reach 0         (1st row )
-            and so on
 
-        then concatenate all rows together
-        */
-        
-        //dir => direction (inc / dec)
         int n = s.size(),  c = 0, dir = 1;
         string ans = "", row[numRows];
         
