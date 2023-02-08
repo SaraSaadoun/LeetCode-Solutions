8f28a7f38d6b4a82a57bed8c2afc1ea3ef66fa2c
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-08 17:22:06 +0200

    Time: 171 ms (80.84%), Space: 71.2 MB (48.12%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,132 +1,37 @@
-//Author: Sara Hamza
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        //approach : simulate the problem
+        //approach: 
+        //sliding window , try to maximize it!
+        //note it is not guaranteed that this sliding window is a valid solution , otherwize it is its length
 
-        //already this is the max no we can get
-        int n = fruits.size();
-        if(n == 1 || n == 2)
-            return n;
+        unordered_map<int, int>baskets;
 
-        //initialize 6 variables , each 3 of them to track one basket (fruit type, fruit no in it, streak till i-th tree)
-        int fruit_type1 = fruits[0], fruit_type2 = fruits[1];
-        int fruits_no_in_basket1 = 1, fruits_no_in_basket2 = 0;
-        int streak_type1 = 1, streak_type2 = 0;
+        //track boundries of the max sliding window so far
+        int l = 0, r = 0, n = fruits.size();
 
-        bool prev_turn = 0; //tracking the previous turn (0=>fruit type 1 || 1=>fruit type 2)
+        while(r < n){
+            //add this fruit type to the baskets
+            //may be it is in one of the two or not
+            baskets[fruits[r]]++;
 
-        int i = 1;
-        //what is the 2nd fruit type? get it
-        for(; i < n; ++i){
-            if(fruits[i] != fruit_type1){
-                fruit_type2 = fruits[i];
-                fruits_no_in_basket2 = streak_type2 = 1;
-                ++i;
-                prev_turn = 1;
-                break;
+            //check if it is not of any type of the two
+            //if not , we will increase the size of this window :) => maximizing
+            if(baskets.size() > 2){
+                
+                //move the whole window to right one step
+                // ++l , ++r
+                //so, remove the type of the leftmost fruit
+                // aready added the type of the rightmost fruit
+                baskets[fruits[l]]--;
+
+                //just after removing, if one basket is empty remove it (because we track the type of fruits in the window using the baskets no)
+                if(baskets[fruits[l]] == 0)
+                    baskets.erase(fruits[l]);
+                ++l;
             }
-
-            fruits_no_in_basket1 = ++streak_type1;
+            ++r;
         }
-
-        // keep maximizing using  max_fruits_no_so_far
-        int max_fruits_no_so_far = fruits_no_in_basket1 + fruits_no_in_basket2;
-
-
-        for(; i < n; ++i){
-            //if it is not one of either fruit types
-            if(fruits[i] != fruit_type1 && fruits[i] != fruit_type2){
-                //first get the max total no of fruits till this point
-                max_fruits_no_so_far = max(max_fruits_no_so_far, fruits_no_in_basket1 + fruits_no_in_basket2);
-                /*
-                    then, keep the basket of the previous fruit type with u
-                        but with the no of consecutive fruits till now only (streak)
-
-                    and reset (empty) the other basket to contain this NEW TYPE!!
-                    and put this new one in it
-                */
-
-                // so, prev fruit is of type 1 ?
-                                    // => change no of fruits of type 1 in the basket to be the no of last consecutive fruits only (streak)
-                                    // => change variables of type 2 to indicate that there is only one fruit inside (current one)
-
-                if(fruits[i-1] == fruit_type1){
-                    //type 2 indicates the current fruit type
-                    fruit_type2 = fruits[i];
-                    fruits_no_in_basket2 = streak_type2 = 1;
-
-                    //type 1 is the same with the last streak
-                    fruits_no_in_basket1 = streak_type1;
-
-                    //mark prev_turn to indicate this current fruit type (for use in the next iteration)
-                    prev_turn = 1; //type 2
-
-                }
-                //same with it of type 2
-                else if (fruits[i-1] == fruit_type2){
-                    //type 1
-                    fruit_type1 = fruits[i];
-                    fruits_no_in_basket1 = streak_type1 = 1;
-                    //type 2
-                    fruits_no_in_basket2 = streak_type2;
-
-                    prev_turn = 0; //type 1
-                }
-                //if it is neither of type 1 nor type 2 =>
-                //              change variables of type 1 to indicate that there is only one fruit inside (current one)
-                //              reset variables of type 2 and its type will be known later (if any fruit then came of a type rather than type 1)
-                else{
-                    //type 1 (current fruit type)
-                    fruit_type1 = fruits[i];
-                    fruits_no_in_basket1 = 1;
-                    streak_type1 = 1;
-
-                    //type 2 (unknown type)
-                    //          - intially mark it also as the current type
-                    //            to force entering this "if section" if different type came later
-                    //            so, type 2 is set correctly
-                    fruit_type2 = fruits[i];
-                    fruits_no_in_basket2 = 0;
-                    streak_type2 = 0;
-
-                    prev_turn = 0; //type 1
-                }
-            }
-            //if this is already one of the 2 types we r collecting
-            //get it in!! and don't forget to reset the streak of the other type
-            //(because this current fruit type will destroy the other one type's streak)
-
-            //type 1 ?
-            else if(fruits[i] == fruit_type1)   {
-                //come into the type 1 basket
-                ++fruits_no_in_basket1;
-                //streak detection depending on the previous fruit type :
-                // still the same as type 1 ? contnuie this streak :)
-                //else start from 1 again :(
-                streak_type1 = (prev_turn == 0 ? streak_type1 + 1: 1);
-
-                //destroy streak of type 2
-                streak_type2 = 0;
-
-                prev_turn = 0; // type 1 turn
-            }
-            else if(fruits[i] == fruit_type2)   {
-                //type 2:
-                ++fruits_no_in_basket2;
-                streak_type2 = (prev_turn == 1 ? streak_type2 + 1 : 1);
-
-                //type 1
-                streak_type1 = 0;
-
-                prev_turn  = 1; // type 2 turn
-            }
-        }
-
-        //maximize for the last time
-        max_fruits_no_so_far = max(max_fruits_no_so_far, fruits_no_in_basket1 + fruits_no_in_basket2);
-
-        return max_fruits_no_so_far;
-
+        return r - l;
     }
 };
