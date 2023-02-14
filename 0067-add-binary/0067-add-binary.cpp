6ae8f870babbe99ab2726d62a0042a74bb94336f
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-14 13:43:23 +0200

    Time: 7 ms (24.93%), Space: 6.8 MB (31.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -6,12 +6,13 @@ public:
         string sum = "";
         if(n > m)
             swap(a, b), swap(n, m);
+        cout << a << ' ' << b ;
         //full adder eq
         int i = n - 1, j = m - 1;
         while(i >= 0){
             bool x = a[i] == '1', y = b[j] == '1';
 
-            sum = to_string((x^y)^cin) + sum;
+            sum = to_string(x^y^cin) + sum;
             cin = (x&y) | (cin&(x^y)); 
 
             --i, --j;
