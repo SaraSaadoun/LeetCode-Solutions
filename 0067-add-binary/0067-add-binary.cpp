8ecdbf140a2fd3cdeea20d040545196c24b27c0b
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-14 11:26:23 +0200

    Time: 7 ms (24.93%), Space: 7 MB (25.07%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int n = a.size(), m = b.size();
+        bool cin = 0;
+        string sum = "";
+        if(n > m)
+            swap(a, b), swap(n, m);
+        cout << a << ' ' << b ;
+        //full adder eq
+        int i = n - 1, j = m - 1;
+        while(i >= 0){
+            bool x = a[i] == '1', y = b[j] == '1';
+
+            sum = to_string((x^y)^cin) + sum;
+            cin = (x&y) | (cin&(x^y)); 
+
+            --i, --j;
+        }
+        //half adder eq
+        while(j >= 0){
+            bool y = b[j] - '0';
+            sum = to_string(y^cin) + sum;
+            cin = y&cin; 
+            --j;
+        }
+        if(cin)
+            sum = "1" + sum;
+
+        return sum;
+        
+    }
+};
