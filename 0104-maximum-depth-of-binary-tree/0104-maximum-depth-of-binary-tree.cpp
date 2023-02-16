dacaa239a87944b2f8d243f278980e436bea591b
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-16 07:40:11 +0200

    Time: 12 ms (48.69%), Space: 19.1 MB (19.79%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -22,7 +22,45 @@ public:
     int maxDepth(TreeNode* root) {
         if(!root)
             return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int levels = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            ++levels;
+            while(sz--){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+                
+            }
+        }
+        return levels;
+    }
+};
+
+/*
+dfs solution:
+class Solution {
+public:
+    int ans = 0;
+    void dfs(TreeNode* node, int level){
+        ans = max(ans, level);
+        if(node->left != nullptr)
+            dfs(node->left, level+1);
+        if(node->right != nullptr)
+            dfs(node->right, level+1);
+    }
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
         dfs(root, 1);
         return ans;
     }
 };
+*/
