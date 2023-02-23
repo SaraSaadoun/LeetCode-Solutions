2e106f9c08d311dfdb6b9574be3eaa9b640131d1
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-23 19:56:38 +0200

    Time: 241 ms (55.49%), Space: 78.1 MB (87.14%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> p,c;
+    int n, OO = 1e9;
+   
+    
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        
+        n = profits.size();
+        
+        vector<pair<int, int>> v(n);
+        for(int i = 0; i < n; ++i){
+            v[i] = {capital[i],  profits[i]};
+        }
+        //sort according to capital
+        sort(v.begin(), v.end());
+
+        //pq : to find the max profit of all possible projects till now in o(1)
+        //possible projects : projects that have capital <= w
+        priority_queue<int> pq;
+
+        int i = 0;
+        while(k--){
+            //get all possible projects
+            while(i<n && v[i].first <= w){
+                pq.push(v[i++].second);
+            }
+            //no one of the projects have the minimum capital (w) to be done :( so break
+            //note from the problem description:
+            // Pick a list of at *most* k distinct projects
+            if(pq.empty())
+                break;
+            
+            //pick the max profit of all possible projects
+            int mx = pq.top();
+            pq.pop();
+            w += mx;
+        }
+        return w;
+    }
+};
