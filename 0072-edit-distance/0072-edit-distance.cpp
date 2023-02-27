073ca899b89a3ff318da28fd757c90e4b7d346ea
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-02-27 06:07:49 +0200

    Time: 175 ms (6.59%), Space: 80.6 MB (13.80%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    map<pair<string, string>, int> mp;
-    int minDistance(string word1, string word2) {
-        if(word1 == word2)
-            return 0;
-        if(word1.size() == 0 )
-            return word2.size();
-        if(word2.size() == 0)
-            return word1.size();
-
-        if(mp[{word1, word2}])
-            return mp[{word1, word2}];
+    int memo[509][509];
+    int n, m;
+    int minDist(int i, int j, string word1, string word2){
         
-        if(word1[0] == word2[0]){
-            
-            mp[{word1, word2}] = minDistance(word1.substr(1), word2.substr(1));
+      
+        if(i == n)
+            return m - j;
+        if(j == m)
+            return n - i;
 
-        }
-        else{
-            string tmp = word1;
-            tmp[0] = word2[0];
-            int choice3 = minDistance(tmp, word2);
-            
-            int choice1 = minDistance(word2.substr(0,1) + word1, word2);
-            int choice2 = minDistance(word1.substr(1), word2);
-            
-            mp[{word1, word2}] =  1 + min(choice1, min(choice2, choice3));
+        int &ret = memo[i][j];
 
-        }
-        return mp[{word1, word2}];   
+        if(~ret)
+            return ret;
+
+        if(word1[i] == word2[j])
+            return ret = minDist(i+1, j+1, word1, word2);
+        
+        return ret = 1 + min({
+            minDist(i+1, j, word1, word2), //delete
+            minDist(i, j+1, word1, word2), //insert
+            minDist(i+1, j+1, word1, word2)}); //replace
+        
+    }
+    int minDistance(string word1, string word2) {
+        n = word1.size(), m = word2.size();
+        memset(memo, -1, sizeof memo);
+
+        return minDist(0, 0, word1, word2);  
     }
 };
