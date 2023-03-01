fd048d37c852d828f187d658a2e31e1509524731
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-03-01 10:49:20 +0200

    Time: 1212 ms (7.58%), Space: 304.1 MB (5.51%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> solve(int st, int end, vector<int>& nums){
+        if(st == end-1)
+            return vector<int>(1, nums[st]);
+
+        int mid = st + (end - st) / 2;
+        vector<int> left = solve(st, mid, nums);
+        vector<int> right = solve(mid, end, nums);
+
+        vector<int> ans;
+        int l = 0, r = 0;
+        int n = left.size(), m = right.size();
+
+        while(l < n && r < m){
+            if(left[l] < right[r])
+                ans.push_back(left[l++]);
+            else
+                ans.push_back(right[r++]);
+
+        }
+        while(l < n)
+            ans.push_back(left[l++]);
+        while(r < m)
+            ans.push_back(right[r++]);
+            
+        return ans;
+
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        return solve(0, nums.size(), nums);
+    }
+};
