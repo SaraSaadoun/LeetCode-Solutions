71a9842888a513b41b097a977fbc9230c555f423
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-03-15 14:45:31 +0200

    Time: 3 ms (84.99%), Space: 10.7 MB (6.36%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,22 +11,12 @@
  */
 class Solution {
 public:
-    
-    int getDepth(TreeNode *node){
-        int len = 0;
-        while(node){
-            ++len;
-            node = node->left;
-        }
-        return len;   
-    }
 
     bool isCompleteTree(TreeNode* root) {
         //accepted Tree => node, node, ... , node, null, null, ... ,null
         
         // BFS
-        queue<TreeNode*> q;
-        q.push(root);
+        queue<TreeNode*> q({root});
         
         // add nodes till the you reeach a node added which = nullptr => stop 
         while(q.front() != nullptr) {
@@ -40,8 +30,6 @@ public:
         while(!q.empty() && q.front() == nullptr)
             q.pop();
         
-        //if there is still nodes which are not null
-        // "not far left as possible"
         return q.empty();
     }
 };
