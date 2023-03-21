93733c5aa88b8a76c340dae1f1d69991fe0b58dc
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-03-21 09:22:44 +0200

    Time: 190 ms (21.57%), Space: 107.6 MB (14.33%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -15,3 +15,27 @@ public:
         return ans;
     }
 };
+
+// another solution :
+
+// class Solution {
+// public:
+//     long long zeroFilledSubarray(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         long long ans = 0, zerosNo = 0;
+        
+//         for (int i = 0; i < n; ++i) {
+//             if(nums[i] == 0) {
+//                 ++zerosNo;
+//             }
+//             else {
+//                 ans += zerosNo * (zerosNo + 1) / 2;
+//                 zerosNo = 0;
+//             }
+//         }
+//         ans += zerosNo * (zerosNo + 1) / 2;
+
+//         return ans;
+//     }
+// };
