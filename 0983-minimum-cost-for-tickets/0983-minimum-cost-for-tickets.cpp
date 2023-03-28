4a69eb7c40db718f1e3ecfdf3e519db73933ebba
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-03-28 15:14:12 +0200

    Time: 3 ms (80.92%), Space: 10 MB (19.98%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //recursive sol
     int solve(int i, set<int>daysSet, vector<int>& costs, vector<int>& memo) {
         if(i > 365)
             return 0;
@@ -20,14 +21,24 @@ public:
         
         return ret;
     }
-    
+    //iterative sol
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+        const int n = 365;
+        vector<int>dp(n + 1, 0);
         set<int>daysSet;
-        for(auto d : days)
+        for(auto d: days)
             daysSet.insert(d);
         
-        vector<int>memo(366, -1);
-        
-        return solve(1, daysSet, costs, memo);
+        for(int i = 1; i <= n; ++i) {
+            if(daysSet.find(i) != daysSet.end()) {
+                int ch1 = dp[max(i-1, 0)] + costs[0]; 
+                int ch2 = dp[max(i-7, 0)] + costs[1];
+                int ch3 = dp[max(i-30, 0)] + costs[2];
+                dp[i] = min({ch1, ch2, ch3});
+            }
+            else 
+                dp[i] = dp[i-1];
+        }
+        return dp[n];
     }
 };
