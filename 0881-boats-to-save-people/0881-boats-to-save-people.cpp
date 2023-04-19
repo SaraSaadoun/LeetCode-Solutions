d97a23c51bdd0f9a0d9af4540b684f89bbc3d004
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-04-19 22:18:56 +0200

    Time: 89 ms (49.42%), Space: 42 MB (60.36%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        int n = people.size();
+        sort(people.begin(), people.end());
+        
+        int l = 0, r = n - 1, ans = 0;
+        while(l <= r) {
+            if(people[l] + people[r] <= limit) {
+                ++l, --r;
+            }
+            else 
+                --r;
+            ++ans;
+        }
+        return ans;
+    }
+        
+};
