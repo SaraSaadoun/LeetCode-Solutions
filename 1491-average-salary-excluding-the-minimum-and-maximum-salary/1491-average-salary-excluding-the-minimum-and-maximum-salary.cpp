92c2c7be452b69f324ad27dd9b983cd0e0cdbdcb
Sara Hamza <68401001+SaraSaadoun@users.noreply.github.com>
2023-05-01 20:55:12 +0300

    Time: 5 ms (23.02%), Space: 7.1 MB (84.06%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    double average(vector<int>& salary) {
-        sort(salary.begin(), salary.end());
+    pair<int, int> get_min_max(vector<int>& salary, int& n) {
+        int mn = salary[0], mx = salary[0];
         
+        for(int i = 1; i < n; ++i) {
+            mn = min(mn, salary[i]);
+            mx = max(mx, salary[i]);
+        }
+        
+        return {mn, mx};
+    }
+    double average(vector<int>& salary) {
         int n = salary.size();
+
+        auto [mn, mx] = get_min_max(salary, n);
+
         double ans = 0;
-        for (int i = 1; i < n - 1; ++i) {
-            ans += salary[i];
+        for (int i = 0; i < n; ++i) {
+            if (salary[i] != mn && salary[i] != mx) {
+                ans += salary[i];
+            }
         }
         ans /= (n - 2);
         
